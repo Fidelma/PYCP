17004400f6d31c388f70991fa0f28ccef5aad0d8
cjmcauley <christopher.mcauley@gmail.com>
2019-10-16 14:22:24 +0100

    spaces removed

--- client/src/containers/PersonContainer.js
+++ client/src/containers/PersonContainer.js
@@ -5,25 +5,16 @@ import PersonList from '../components/person/PersonList';
 class PersonContainer extends Component {
   constructor(props){
     super(props);
-
   }
 
-render(){
-  return(
-    <>
-    <h2>Person Container</h2>
-    <PersonList people={this.props.people}/>
-    </>
-  )
-}
-
-
-
-
-
-
-
-
+  render(){
+    return(
+      <>
+      <h2>Person Container</h2>
+      <PersonList people={this.props.people}/>
+      </>
+    )
+  }
 }
 
 export default PersonContainer;
