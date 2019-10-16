149de9d8801ea57e13edbccfed06e58b069e4bac
mikefallon <mikefallon81@gmail.com>
2019-10-16 09:34:13 +0100

    Person Container set-up

---
+++ client/src/containers/PersonContainer.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class PersonContainer extends Component {
+  constructor(props){
+    super(props);
+
+  }
+
+render(){
+  return(
+    <>
+    <h2>Person Container</h2>
+    </>
+  )
+}
+
+
+
+
+
+
+
+
+}
+
+export default PersonContainer;
