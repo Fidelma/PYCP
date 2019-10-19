7d30e450d1496745e17dedbb38908f42a4e38f50
mikefallon <mikefallon81@gmail.com>
2019-10-19 14:19:56 +0100

    changed buttons to be inside links

---
+++ client/src/components/home/Button.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../styles/buttonStyles.css';
+
+const Button = () => (
+<container id='buttons'>
+
+<Link to='/registration'>
+<button className='mainBtn'>
+<h2>New Registration</h2>
+<p>Create new consent form record.</p>
+</button>
+</Link>
+
+<Link to='/contact'>
+<button className='mainBtn'>
+<h2>Contact Details</h2>
+<p>Contact details for children</p>
+</button>
+</Link>
+
+<Link to='/activities'>
+<button className='mainBtn'>
+<h2>Activity Details</h2>
+<p>View activities and create, edit or delete.</p>
+</button>
+</Link>
+
+<Link to='/people'>
+<button className='mainBtn'>
+<h2>Register</h2>
+<p>View details and filter reports</p>
+</button>
+</Link>
+
+
+</container>
+);
+
+export default Button;
