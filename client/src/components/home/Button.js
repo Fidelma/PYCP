23101acc09bd2896255a0752386d45493e55f080
cjmcauley <christopher.mcauley@gmail.com>
2019-10-21 09:24:36 +0100

    Home buttons styled

--- client/src/components/home/Button.js
+++ client/src/components/home/Button.js
@@ -7,29 +7,29 @@ const Button = () => (
 
 <Link to='/registration'>
 <button className='mainBtn'>
-<h2>New Registration</h2>
-<p>Create new consent form record.</p>
+<h2 className='BtnH2'>New Registration</h2>
+<p className='BtnP'>Create new consent form record.</p>
 </button>
 </Link>
 
 <Link to='/contact'>
 <button className='mainBtn'>
-<h2>Contact Details</h2>
-<p>Contact details for children</p>
+<h2 className='BtnH2'>Contact Details</h2>
+<p className='BtnP'>Contact details for children</p>
 </button>
 </Link>
 
 <Link to='/activities'>
 <button className='mainBtn'>
-<h2>Activity Details</h2>
-<p>View activities and create, edit or delete.</p>
+<h2 className='BtnH2'>Activity Details</h2>
+<p className='BtnP'>View activities and create, edit or delete.</p>
 </button>
 </Link>
 
 <Link to='/people'>
 <button className='mainBtn'>
-<h2>Register</h2>
-<p>View details and filter reports</p>
+<h2 className='BtnH2'>Register</h2>
+<p className='BtnP'>View details and filter reports</p>
 </button>
 </Link>
 
