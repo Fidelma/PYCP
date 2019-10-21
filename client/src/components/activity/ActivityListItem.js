2ab9c2172f901969aee86682c6fa35b85b07a0a2
cjmcauley <christopher.mcauley@gmail.com>
2019-10-21 13:26:48 +0100

    removed ages

--- client/src/components/activity/ActivityListItem.js
+++ client/src/components/activity/ActivityListItem.js
@@ -5,13 +5,12 @@ const ActivityListItem = ({activity}) => (
 
     <tr>
      <td>{activity.title}</td>
-     <td>{activity.age}</td>
      <td>{activity.startTime} - {activity.endTime}</td>
      <td>{activity.day}</td>
      <td><button>Edit Activity</button></td>
      <td><button>Delete Activity</button></td>
     </tr>
-  
+
   </>
 
 )
