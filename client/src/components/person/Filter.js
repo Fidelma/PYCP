08e76a1a91c151dec87d4678b68a56d132ea54e6
gavinblack87 <gavin_black@hotmail.co.uk>
2019-10-21 13:30:06 +0100

    added checkboxes, not functional

--- client/src/components/person/Filter.js
+++ client/src/components/person/Filter.js
@@ -27,7 +27,38 @@ class Filter extends Component {
         <label>Filter by Name or Year</label>
         <input type="text" value={this.state.filter} onChange={this.updateFilter}/><br/>
 
-        
+        <div>
+          <label for="P1">P1</label>
+          <input type="checkbox" name="yearGroup" value="P1" onClick={this.updateFilter}/>
+          
+          <label for="P3">P2</label>
+          <input type="checkbox" name="yearGroup" value="P3" onClick={this.updateFilter}/>
+          
+          <label for="P4">P4</label>
+          <input type="checkbox" name="yearGroup" value="P4" onClick={this.updateFilter}/>
+          
+          <label for="P5">P5</label>
+          <input type="checkbox" name="yearGroup" value="P5" onClick={this.updateFilter}/>
+          
+          <label for="P6">P6</label>
+          <input type="checkbox" name="yearGroup" value="P6" onClick={this.updateFilter}/>
+          
+          <label for="P7">P7</label>
+          <input type="checkbox" name="yearGroup" value="P7" onClick={this.updateFilter}/>
+          
+          <label for="S1">S1</label>
+          <input type="checkbox" name="yearGroup" value="S1" onClick={this.updateFilter}/>
+          
+          <label for="S2">S2</label>
+          <input type="checkbox" name="yearGroup" value="S2" onClick={this.updateFilter}/>
+          
+          <label for="S3">S3</label>
+          <input type="checkbox" name="yearGroup" value="S3" onClick={this.updateFilter}/>
+          
+          <label for="S4">S4</label>
+          <input type="checkbox" name="yearGroup" value="S4" onClick={this.updateFilter}/>
+          
+        </div>
 
         <div>
         <table className="contact-table">
