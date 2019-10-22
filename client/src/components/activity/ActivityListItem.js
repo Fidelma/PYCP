a1af4df45a02b206859f5a686299bf8d89a43129
mikefallon <mikefallon81@gmail.com>
2019-10-22 13:54:21 +0100

    Edit activity btn displaying form and update btn.

--- client/src/components/activity/ActivityListItem.js
+++ client/src/components/activity/ActivityListItem.js
@@ -6,20 +6,22 @@ const ActivityListItem = (props) => {
     props.deleteActivity(props.activity._id)
   }
 
+  function handleEdit(){
+    props.editActivity(props.activity)
+  }
+
 
 
   return(
   <>
-
     <tr>
      <td>{props.activity.title}</td>
      <td>{props.activity.age.join(', ')}</td>
      <td>{props.activity.startTime} - {props.activity.endTime}</td>
      <td>{props.activity.day}</td>
-     <td><button>Edit Activity</button></td>
+     <td><button onClick={handleEdit}>Edit Activity</button></td>
      <td><button onClick={handleDelete}>Delete Activity</button></td>
     </tr>
-
   </>
 
 )
--- client/src/containers/ActivityContainer.js
+++ client/src/containers/ActivityContainer.js
@@ -7,19 +7,35 @@ import NavBar from '../components/navigation/NavBar';
 class ActivityContainer extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      edit: false,
+      activityToEdit: null
+    }
+
+    this.editActivity = this.editActivity.bind(this);
+
   }
 
-
+editActivity(activity){
+ this.setState({activityToEdit: activity, edit: !this.state.edit})
+ this.props.toggleActivityForm();
+}
 
   render(){
     return(
       <>
       <NavBar/>
       <h2>Activity Container</h2>
-      <ActivityList activities={this.props.activities} deleteActivity={this.props.deleteActivity}/>
+      <ActivityList
+      activities={this.props.activities} deleteActivity={this.props.deleteActivity}
+      editActivity={this.editActivity}
+      />
       <NewActivityButton  toggleActivityForm={this.props.toggleActivityForm}/>
-      <ActivityForm addActivity={this.props.addActivity}
-      displayActivityForm={this.props.displayActivityForm}/>
+      <ActivityForm
+      addActivity={this.props.addActivity}
+      displayActivityForm={this.props.displayActivityForm}
+      edit={this.state.edit}
+      activityToEdit={this.state.activityToEdit}/>
       </>
     )
   }
