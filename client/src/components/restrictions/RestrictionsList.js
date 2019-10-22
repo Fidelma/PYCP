4e1a9b6cd65a13abe88e02c0289b130cb8f6ea7d
bchutchison <benhutchison85@gmail.com>
2019-10-22 19:47:11 +0100

    restrictions - list of children with timeOuts added

--- client/src/components/restrictions/RestrictionsList.js
+++ client/src/components/restrictions/RestrictionsList.js
@@ -2,19 +2,26 @@ import React, {Component} from 'react';
 
 const RestrictionsList = ({person}) => {
 
+  const RestrictionList = (props) => {
+    if(props.person.timeOut.exists) {
+      if(props.person.timeOut.startDate) {
+      const startDate = props.person.timeOut.startDate;
+      let startDateReduced = startDate.slice(1,10);
+      const endDate = props.person.timeOut.endDate;
+      let endDateReduced = endDate.slice(1,10);
+      return <tr><td>{props.person.name.firstName}</td><td>{props.person.name.lastName}</td><td>{startDateReduced}</td><td>{endDateReduced}</td><td>{props.person.timeOut.reason}</td></tr>
+      }
+    }
+    return null
+  }
+
 
 
 
 
     return(
       <>
-      <tr>
-        <td>{person.name.firstName}</td>
-        <td>{person.name.lastName}</td>
-        <td>{person.timeOut.startDate}</td>
-        <td>{person.timeOut.endDate}</td>
-        <td>{person.timeOut.reason}</td>
-      </tr>
+        <RestrictionList person={person}/>
       </>
     )
 }
