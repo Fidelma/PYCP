07bc6ead2f5d0e840fd30fe4c370d974a1204e5f
bchutchison <benhutchison85@gmail.com>
2019-10-23 09:51:56 +0100

    restrictions list displayed

--- client/src/components/restrictions/RestrictionsList.js
+++ client/src/components/restrictions/RestrictionsList.js
@@ -4,20 +4,16 @@ const RestrictionsList = ({person}) => {
 
   const RestrictionList = (props) => {
     if(props.person.timeOut.exists) {
-      if(props.person.timeOut.startDate) {
+      if(props.person.timeOut.endDate) {
       const startDate = props.person.timeOut.startDate;
-      let startDateReduced = startDate.slice(1,10);
+      let startDateReduced = startDate.slice(0,10);
       const endDate = props.person.timeOut.endDate;
-      let endDateReduced = endDate.slice(1,10);
+      let endDateReduced = endDate.slice(0,10);
       return <tr><td>{props.person.name.firstName}</td><td>{props.person.name.lastName}</td><td>{startDateReduced}</td><td>{endDateReduced}</td><td>{props.person.timeOut.reason}</td></tr>
       }
     }
     return null
   }
-
-
-
-
 
     return(
       <>
@@ -27,3 +23,6 @@ const RestrictionsList = ({person}) => {
 }
 
 export default RestrictionsList;
+
+
+//MOMENT.JS
