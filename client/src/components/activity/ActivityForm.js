6fb16a89ee2886386ec21ef175994a45f284330e
mikefallon <mikefallon81@gmail.com>
2019-10-24 11:07:21 +0100

    style file added for activity form

--- client/src/components/activity/ActivityForm.js
+++ client/src/components/activity/ActivityForm.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import activityForm from '../../styles/activityForm.css';
 
 class ActivityForm extends Component {
 
@@ -36,18 +37,18 @@ render(){
 
 
 
-  <form>
+  <form className='new-activity-form'>
   <fieldset>
-    <fieldset>
+
     <div>
-      <label>Activity Title</label>
+      <label className='activity-table'>Activity Title</label>
       <input type="text" id="title" name="title" value={this.props.activity.title} onChange={this.handleChange} required/>
     </div>
-    </fieldset>
 
-    <fieldset>
+
+
     <div>
-      <label>Day</label>
+      <label className='activity-table'>Day</label>
       <select name="day" id="day" value={this.props.activity.day} onChange={this.handleChange} required>
         <option value="" defaultValue disabled hidden>Select Here</option>
         <option value="Monday">Monday</option>
@@ -61,31 +62,35 @@ render(){
     </div>
 
     <div>
-      <label>Start Time</label>
+      <label className='activity-table'>Start Time</label>
       <input type="time" id="startTime" name="startTime" value={this.props.activity.startTime} onChange={this.handleChange} required/>
     </div>
 
     <div>
-      <label>End Time</label>
+      <label className='activity-table'>End Time</label>
       <input type="time" id="endTime" name="endTime" value={this.props.activity.endTime} onChange={this.handleChange} required/>
     </div>
-    </fieldset>
 
-    <fieldset>
+
+
       <div>
-        <ul>
           {this.props.activity.ageCheckboxes.map((age, i) => {
             return(
-          	<li key={i}>
+              <>
+
+          	<li className='checks' key={i}>
               {age.year}
-          	  <input value={age.checked} checked={age.checked} type="checkbox" onChange={this.props.onToggle.bind(this, i)} />
+          	  <input value={age.checked}
+
+               checked={age.checked} type="checkbox" onChange={this.props.onToggle.bind(this, i)} />
           	</li>
+            </>
           )})}
-        </ul>
+
       </div>
 
     <div>
-      <label>Gender</label>
+      <label className='activity-table'>Gender</label>
       <select name="gender" id="gender" value={this.props.activity.gender} onChange={this.handleChange} required>
         <option value="" defaultValue disabled hidden>Select Here</option>
         <option value="Male">Male</option>
@@ -96,15 +101,16 @@ render(){
     </div>
 
     <div>
-      <label>Location</label>
+      <label className='activity-table'>Location</label>
       <input type="text" id="location" name="location" value={this.props.activity.location} onChange={this.handleChange} required/>
     </div>
 
     <div>
-      <label>Description</label>
+      <label className='activity-table'>Description</label>
       <textarea id="description" name="description" value={this.props.activity.description} onChange={this.handleChange} required>
       </textarea>
     </div>
+
 
 
     <div style={{ display: this.props.edit ? 'block' : 'none'}
@@ -119,7 +125,6 @@ render(){
 
 
 
-    </fieldset>
     </fieldset>
   </form>
 )
