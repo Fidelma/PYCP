de2ff70264340f704cbec0991809985ef04360a4
mikefallon <mikefallon81@gmail.com>
2019-10-25 10:55:25 +0100

    commented out splice code in update activity

--- client/src/containers/HomePageContainer.js
+++ client/src/containers/HomePageContainer.js
@@ -397,10 +397,10 @@ class HomePageContainer extends Component {
   updateActivity(id, activity){
     const request = new ActivityRequest
     request.edit(id, activity)
-    const tempActivities = this.state.activities
-    const index = tempActivities.indexOf(id);
-    tempActivities.splice(index, 1, activity);
-    this.setState({activities: tempActivities})
+    // const tempActivities = this.state.activities
+    // const index = tempActivities.indexOf(id);
+    // tempActivities.splice(index, 1, activity);
+    // this.setState({activities: tempActivities})
   }
 
   deleteActivity(id){
